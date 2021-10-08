c5fbffa532e489f37e6a85cf43779170cea56461
kaleabm18 <kaleabm18@gmail.com>
2021-10-07 23:32:26 -0700

    new controller add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,18 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class UserController extends Controller
 {
- /**
-  *   @return void()
-  *
-
-  */
 
 public function index()
     {
       //return "welcome to user page";
-  return view('go');
+
+     //return users;
+     /*
+     $users=new User();
+     $users->name="mekdi";
+     $users->email="ka@gmail.com";
+     $users->password="789456";
+     $users->save();
+*/
+$data=[
+'name'=>'kaleab',
+'email'=>'k@gmail.com',
+'password'=>'password',
+];
+User::create($data);
+$user=User::all();
+return $user;
+
+    // return view('go');
     }
 }
